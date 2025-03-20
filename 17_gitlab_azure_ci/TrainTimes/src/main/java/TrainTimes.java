f794e69dd26601b82de6b07fa41a0f8aa22baf46
Peadar Grant <Peadar.Grant@dkit.ie>
2025-03-20 22:46:15 +0000

    change

---
+++ 17_gitlab_azure_ci/TrainTimes/src/main/java/TrainTimes.java
@@ -0,0 +1,9 @@
+public class TrainTimes {
+
+    public static void main(String[] args) {
+        System.out.println("This app hasn't been finished yet!");
+        System.out.println("It will be, soon!");
+    }
+
+}
+
